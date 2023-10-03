8b0c6edb40d5e8dbc77b71974074c50495f773db
Nikulshin <nikulshin.as@phystech.edu>
2023-10-03 19:55:38 +0300

    damn shell-wrapper

---
+++ shell.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+int main(){
+
+	while(1){
+
+		char cmd[1024];
+		char *args[1024] = {0};
+
+		int size = 0;
+
+		if ((size = read(0, cmd, 1024)) == -1){
+			printf("Read error\n");
+			return -1;
+		}
+
+		cmd[size] = '\0';
+
+		pid_t pid = fork();
+
+		if (pid < 0){
+			printf("Fork error, pid = %d\n", pid);
+			return -1;
+		}      
+
+		if (pid){
+			int status;
+			waitpid(pid, &status, 0);
+			printf("Ret code: %d\n", WEXITSTATUS(status));
+			continue;
+		}
+
+		else if (read(0, cmd, 1024) >= 0){
+			char delim[] = "\n ";
+
+			args[0] = strtok(cmd, delim);
+			int i = 0;
+			
+			for (i = 1; i < 1024 && (args[i] = strtok(cmd, delim)) != NULL; i++);
+
+			args[i] = NULL;
+			
+			for (int j = 0; j < 1024; j++)
+				printf("%s | ", args[j]);
+
+			if (execvp(args[0], args) == -1){
+				printf("Exec error\n");
+				return -1;
+			}
+		}
+	}
+}
